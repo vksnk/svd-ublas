e8c0d1497515851e36abe8b8b11b27a582c6db60
Volodymyr Kysenko <rizen.wp@gmail.com>
2011-08-05 23:57:08 +0300

    Added folder data/

--- main.cpp
+++ main.cpp
@@ -13,16 +13,13 @@ int main() {
     ublas::matrix<float> in;
     ublas::vector<float> v;
 
-    f.open("wiki.example", std::fstream::in);
+    f.open("data/wiki.example", std::fstream::in);
     f >> in;
-    f >> v;
+
     f.close();
 
-    ublas::vector<float> p = ublas::prod(v, in);
 
     std::cout << in << "\n";
-    std::cout << v << "\n";
-    std::cout << p << "\n";
 
 	return 0;
 }

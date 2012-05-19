450a5a1739e86b040d6b0e643a9e52320badcc44
Volodymyr Kysenko <rizen.wp@gmail.com>
2012-05-20 00:36:45 +0300

    -- Reduction to Hessenberg form

--- eigen.cpp
+++ eigen.cpp
@@ -3,6 +3,7 @@
 void
 householder(ublas::matrix<float>& A,
         ublas::matrix <float>& QL,
+        ublas::matrix <float>& QR,
         unsigned int row_start, 
         unsigned int col_start)
 {
@@ -72,6 +73,25 @@ householder(ublas::matrix<float>& A,
             QL(i, j) = QL(i, j) - 2 * v(j) * sum_Qv;
     }
 
+    for (unsigned int i = 0; i < A.size1(); i++) {
+        float sum_Av = 0.0f;
+
+        for (unsigned int j = 0; j < A.size2(); j++)
+            sum_Av = sum_Av + (v(j) * A(i, j));
+        for (unsigned int j = 0; j < A.size2(); j++)
+            A(i, j) = A(i, j) - 2 * v(j) * sum_Av;
+    }
+
+    for (unsigned int i = 0; i < A.size2(); i++) {
+        float sum_Qv = 0.0f;
+
+        for (unsigned int j = 0; j < A.size2(); j++)
+            sum_Qv = sum_Qv + (v(j) * QR(i, j));
+        for (unsigned int j = 0; j < A.size2(); j++)
+            QR(i, j) = QR(i, j) - 2 * v(j) * sum_Qv;
+    }
+
+
 }
 
 void eigen(ublas::matrix < float >&A,
@@ -92,21 +112,53 @@ void eigen(ublas::matrix < float >&A,
     unsigned int to = std::min(row_num, col_num);
 
     for (unsigned int i = 0; i < to; i++) {
-        householder(A, QQL, i + 1, i);
+        householder(A, QQL, QQR, i + 1, i);
     }
+}
+
+bool check_tridiag(ublas::matrix<float>& A)
+{
+    const float EPS = 0.0001f;
+
+    for (unsigned int i = 0; i < A.size1(); i++) {
+        for (unsigned int j = 0; j < A.size2(); j++) {
+            if ((std::abs(A(i, j)) > EPS) && ((i - 1) != j) && (i != j) && ((i + 1) != j))
+            {
+                std::cout << "Failed at " << i << " " << j << " " << A(i, j) << "\n";
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+bool check_hessenberg(ublas::matrix<float>& A)
+{
+    const float EPS = 0.0001f;
+
+    for (int i = 0; i < A.size1(); i++) {
+        for (int j = 0; j < A.size2(); j++) {
+            if ((std::abs(A(i, j)) > EPS) && (i > (j + 1)))
+            {
+                std::cout << "Failed at " << i << " " << j << " " << A(i, j) << "\n";
+                return false;
+            }
+        }
+    }
+    return true;
 }
 
 int main()
 {
     ublas::matrix < float > in;
-
+/*
     std::fstream f;
     f.open("data/wiki.example", std::fstream::in);
     f >> in;
     f.close();
-
-//  srand((unsigned int)time(0));
-//  random_fill(in, 250, 150);
+*/
+    srand((unsigned int)time(0));
+    random_fill(in, 12, 12);
 
     ublas::matrix < float > ref = in;
 
@@ -118,7 +170,15 @@ int main()
 
     eigen(in, QQL, QQW, QQR);
 
+    ublas::matrix < float > result;
+
+    result = ublas::prod(in, trans(QQR));
+    result = ublas::prod(QQL, result);
+
     pretty_print("Tridiag:", in);
 
+    std::cout << "DIFF = " << matrix_compare(result, ref) << "\n";
+    std::cout << "Is Hessenberg ? " << check_hessenberg(in) << "\n";
+    std::cout << "Is tridiag ? " << check_tridiag(in) << "\n";
     return 0;
 }
